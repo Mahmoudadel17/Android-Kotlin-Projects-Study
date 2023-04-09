698f89eacb16d7a89f6d927c3ef84225c0354022
mahmoud adel <mahmoudadel5556@gmail.com>
2023-04-09 15:03:48 +0200

    start write backend code

--- SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.splashscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var lastNumeric = false
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    fun onAllclearClic(view: android.view.View) {}
+
+    fun onEqualClic(view: android.view.View) {}
+
+    fun onOperatorClic(view: android.view.View) {}
+
+    fun onClearClic(view: android.view.View) {}
+
+    fun onBackClic(view: android.view.View) {}
+
+    fun onDigitClic(view: android.view.View) {}
+
+
 }
