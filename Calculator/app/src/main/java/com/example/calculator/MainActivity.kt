82b2310c60ccc5ebda847a966b95b9647ef051d9
mahmoud adel <mahmoudadel5556@gmail.com>
2023-04-10 04:46:13 +0200

    add calculator project

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.splashscreen
+package com.example.calculator
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import com.example.splashscreen.databinding.ActivityMainBinding
-import com.example.splashscreen.
+import com.example.calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.ArithmeticException
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     var stateError = false
     var lastDot = false
 
-    private lateinit var expression:Expression
+    private lateinit var expression: Expression
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    fun onAllclearClic(view: android.view.View) {
+    fun onAllclearClic(view: View) {
         binding.dataTv.text = ""
         binding.resultTv.text = ""
         stateError = false
@@ -38,13 +38,13 @@ class MainActivity : AppCompatActivity() {
         binding.resultTv.visibility = View.GONE
     }
 
-    fun onEqualClic(view: android.view.View) {
+    fun onEqualClic(view: View) {
         onEqual()
-        binding.dataTv.text = binding.resultTv.text.toString()
+        binding.dataTv.text = binding.resultTv.text.toString().drop(1)
     }
 
-    fun onOperatorClic(view: android.view.View) {
-        if (!stateError && lastNumeric){
+    fun onOperatorClic(view: View) {
+        if (!stateError && lastNumeric) {
             binding.dataTv.append((view as Button).text)
             lastDot = false
             lastNumeric = false
@@ -52,35 +52,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClearClic(view: android.view.View) {
+    fun onClearClic(view: View) {
         binding.dataTv.text = ""
         lastNumeric = false
     }
 
-    fun onBackClic(view: android.view.View) {
+    fun onBackClic(view: View) {
         binding.dataTv.text = binding.dataTv.text.toString().dropLast(1)
 
         try {
             val lastchar = binding.dataTv.text.toString().last()
 
-            if (lastchar.isDigit()){
+            if (lastchar.isDigit()) {
                 onEqual()
             }
-        }catch (e:Exception){
+        } catch (e: Exception) {
             binding.resultTv.text = ""
             binding.resultTv.visibility = View.GONE
-            Log.e("last char error",e.toString())
+            Log.e("last char error", e.toString())
         }
     }
 
-    fun onDigitClic(view: android.view.View) {
+    fun onDigitClic(view: View) {
 
-        if (stateError){
-            binding.dataTv.text = (view as  Button).text
+        if (stateError) {
+            binding.dataTv.text = (view as Button).text
             stateError = false
 
-        }else{
-            binding.dataTv.append((view as  Button).text)
+        } else {
+            binding.dataTv.append((view as Button).text)
 
         }
         lastNumeric = true
@@ -88,8 +88,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun onEqual(){
-        if (lastNumeric && !stateError){
+    fun onEqual() {
+        if (lastNumeric && !stateError) {
             val txt = binding.dataTv.text.toString()
 
             expression = ExpressionBuilder(txt).build()
@@ -102,15 +102,13 @@ class MainActivity : AppCompatActivity() {
                 binding.resultTv.text = "=$result"
 
 
-
-            }catch (ex:ArithmeticException){
-                Log.e("evaluate error",ex.toString())
+            } catch (ex: ArithmeticException) {
+                Log.e("evaluate error", ex.toString())
                 binding.resultTv.text = "Error"
                 stateError = true
                 lastNumeric = false
             }
         }
     }
-
-
 }
+
--- SplashScreen/app/src/main/java/com/example/splashscreen/Splash.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/Splash.kt
@@ -1,6 +1,8 @@
 package com.example.splashscreen
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -10,7 +12,17 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler().postDelayed({
+            val configuration = Resources.getSystem().configuration
+            val uiModeFlags = configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            val isNightMode = uiModeFlags == Configuration.UI_MODE_NIGHT_YES
             val splash = Intent(this,MainActivity::class.java)
+//            if (isNightMode) {
+//                // The device is in dark mode
+//                splash = Intent(this,MainActivity2::class.java)
+//            } else {
+//                // The device is not in dark mode
+//                splash = Intent(this,MainActivity::class.java)
+//            }
             startActivity(splash)
         },4000)
     }
